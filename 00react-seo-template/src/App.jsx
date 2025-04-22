4c1791ba1b940b6a0c97488aa8438ebba3b2de5a
codingclap <codingclap@gmail.com>
2025-04-22 17:09:51 +0530

    configure router

---
+++ 00react-seo-template/src/App.jsx
@@ -0,0 +1,30 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import React from "react";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>
+  },
+  {
+    path: "about",
+    element: <About/>
+  },
+  {
+    path: "contact",
+    element: <Contact/> 
+  },
+]);
+
+function App() { 
+  return (
+    <>
+      <RouterProvider router={router}> </RouterProvider>
+    </>
+  )
+}
+
+export default App;
---
+++ 00react-seo-template/src/pages/Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contact = () => {
+     return (
+          <div>
+               <h1>Contact Us</h1>
+               <p>Welcome to the Contact page. Here you can learn more about our mission and values.</p>
+          </div>
+     );
+};
+
+export default Contact;
